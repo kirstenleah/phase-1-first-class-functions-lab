ec542867598146996f0fbc128c7dada468de7cef
Kirsten <kirsten.staggs@gmail.com>
2022-04-05 20:55:52 -0600

    commit tree cf664dbcf63f7a5fa8adc0bcfea715e25a073775

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(anArray) {
+    return anArray.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(anArray) {
+    return anArray.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplyFare) {
+    return function (fare) {
+        return multiplyFare * fare;
+    };
+};
+
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (anArray, driversToReturn) {
+    return driversToReturn(anArray);
+};
